fffea8e57a711f78bea4a4075ca7f91a16dc7345
Joshua Alphonse <joshalphonse@gmail.com>
2019-07-06 19:38:49 -0400

    currentlocation added

--- Shleepy-AR/ViewController.swift
+++ Shleepy-AR/ViewController.swift
@@ -7,14 +7,67 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
-
+    
+    private let locationManager = CLLocationManager()
+    private var currentCoordinate: CLLocationCoordinate2D?
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureLocationServices()
         // Do any additional setup after loading the view.
     }
-
+    
+    private func configureLocationServices(){
+        locationManager.delegate = self
+        
+        let status = CLLocationManager.authorizationStatus()
+        
+        if status == .notDetermined{
+            locationManager.requestAlwaysAuthorization()
+        }else if status == .authorizedAlways || status == .authorizedWhenInUse {
+          beginLocationUpdates(locationManager: locationManager)
+            
+        }
+    }
+    
+    private func beginLocationUpdates(locationManager: CLLocationManager){
+        mapView.showsUserLocation = true
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+        
+    }
+    private func zoomToLatestLocation(with coordinate: CLLocationCoordinate2D){
+        
+        let zoomregion = MKCoordinateRegion(center: coordinate, latitudinalMeters: 1000, longitudinalMeters: 1000)
+        mapView.setRegion(zoomregion, animated: true)
+    }
 
 }
+
+
+extension ViewController: CLLocationManagerDelegate{
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    print("Did get latest location")
+        guard let latestLocation = locations.first else { return}
+        if currentCoordinate == nil{
+       zoomToLatestLocation(with: latestLocation.coordinate)
+        }
+        
+        currentCoordinate = latestLocation.coordinate
+    }
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+    print("the status changed")
+        if status == .authorizedAlways || status == .authorizedWhenInUse {
+            beginLocationUpdates(locationManager: locationManager)
+            beginLocationUpdates(locationManager: locationManager)
+        }
+    }
+}
+
+
 
