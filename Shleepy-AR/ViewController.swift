c88eb3889e2183749746388e76b8c1a44b3f9729
Joshua Alphonse <joshalphonse@gmail.com>
2019-07-06 17:31:28 -0400

    Initial Commit

---
+++ Shleepy-AR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Shleepy-AR
+//
+//  Created by Joshua Alphonse on 7/6/19.
+//  Copyright © 2019 com.joshua. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
